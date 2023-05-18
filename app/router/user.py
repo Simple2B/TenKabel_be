87670dcda93346d32180c56af26aa202b00d9b73
BohdanSizomin <bohdansizomin@gmail.com>
2023-05-18 18:14:09 +0300

    get user profile get certain user profile

--- app/router/user.py
+++ app/router/user.py
@@ -1,8 +1,11 @@
-from fastapi import HTTPException, Depends, APIRouter
+from fastapi import HTTPException, Depends, APIRouter, status
+
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 import app.model as m
 import app.schema as s
+from app.logger import log
 from app.dependency import get_current_user
 from app.database import get_db
 
@@ -19,15 +22,27 @@ def create_user(user: s.User, db: Session = Depends(get_db)):
     return new_user
 
 
-@user_router.get("/{id}", response_model=s.User)
-def get_user(
-    id: int,
+@user_router.get("", response_model=s.User)
+def get_current_user_profile(
     db: Session = Depends(get_db),
     current_user: int = Depends(get_current_user),
 ):
-    user = db.query(m.User).get(id)
+    return current_user
 
+
+@user_router.get("/{user_uuid}", response_model=s.User)
+def get_user_profile(
+    user_uuid: str,
+    db: Session = Depends(get_db),
+):
+    user: m.User = db.scalars(select(m.User).where(m.User.uuid == user_uuid)).first()
     if not user:
-        raise HTTPException(status_code=404, detail="This user was not found")
-
+        log(
+            log.ERROR,
+            "User %s not found",
+            user_uuid,
+        )
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+        )
     return user
--- tests/test_routes/test_user.py
+++ tests/test_routes/test_user.py
@@ -30,3 +30,32 @@ def test_signup(client: TestClient, db: Session, test_data: TestData):
     assert db.execute(
         sa.select(m.User).where(m.User.email == test_data.test_user.email)
     ).one()
+
+
+def test_get_user_profile(
+    client: TestClient,
+    db: Session,
+    test_data: TestData,
+    authorized_users_tokens: list[s.Token],
+):
+    response = client.get(
+        "api/user",
+        headers={
+            "Authorization": f"Bearer {authorized_users_tokens[0].access_token}",
+        },
+    )
+    assert response.status_code == 200
+    resp_obj: s.User = s.User.parse_obj(response.json())
+    user: m.User = (
+        db.query(m.User)
+        .filter_by(email=test_data.test_authorized_users[0].email)
+        .first()
+    )
+    assert user
+    assert resp_obj.uuid == user.uuid
+
+    # get user by uuid
+    response = client.get(
+        f"api/user/{user.uuid}",
+    )
+    assert response.status_code == 200
