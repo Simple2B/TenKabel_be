838b0c6bf8a0b789c30b7f7ded60c45602af0ac8
Anton <tinkerino571@gmail.com>
2023-05-22 14:48:47 +0300

    added google auth

--- tests/test_routes/test_user.py
+++ tests/test_routes/test_user.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+from fastapi import status
 import sqlalchemy as sa
 from sqlalchemy.orm import Session
 
@@ -30,3 +31,49 @@ def test_signup(client: TestClient, db: Session, test_data: TestData):
     assert db.execute(
         sa.select(m.User).where(m.User.email == test_data.test_user.email)
     ).one()
+
+
+def test_google_auth(client: TestClient, db: Session, test_data: TestData) -> None:
+    user: m.User = (
+        db.query(m.User).filter_by(email=test_data.test_users[0].email).first()
+    )
+    assert user
+
+    request_data = s.BaseUser(
+        email=user.email,
+        password=test_data.test_users[0].password,
+        username=user.username,
+        google_openid_key=user.google_openid_key,
+        picture=user.picture,
+        phone=user.phone,
+    ).dict()
+
+    response = client.post("api/auth/google", json=request_data)
+    assert response.status_code == status.HTTP_200_OK
+    resp_obj = s.Token.parse_obj(response.json())
+    assert resp_obj.access_token
+
+    # checking non existing user
+    user = db.query(m.User).filter_by(email=test_data.test_user.email).first()
+    assert not user
+
+    request_data = s.BaseUser(
+        email=test_data.test_user.email,
+        password=test_data.test_user.password,
+        username=test_data.test_user.username,
+        google_openid_key=test_data.test_user.google_openid_key,
+        picture=test_data.test_user.picture,
+        phone=test_data.test_user.phone,
+    ).dict()
+
+    response = client.post("api/auth/google", json=request_data)
+    assert response.status_code == status.HTTP_200_OK
+    resp_obj = s.Token.parse_obj(response.json())
+    assert resp_obj.access_token
+
+    # checking if the user has created
+    user = db.query(m.User).filter_by(email=test_data.test_user.email).first()
+    assert user
+
+    response = client.post("api/auth/google", json=request_data)
+    assert response.status_code == 200
