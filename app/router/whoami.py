209c1dca3c700321aea97f3de0ee371cb22d185c
Taraslysak <lysak.ipr@gmail.com>
2023-06-06 19:33:41 +0300

    user uuid as whoami response

--- app/router/whoami.py
+++ app/router/whoami.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, status, Depends
 
 import app.model as m
+import app.schema as s
 from app.dependency import get_current_user
 
 whoami_router = APIRouter(prefix="/whoami", tags=["Whoami"])
 
 
-@whoami_router.get("/user", status_code=status.HTTP_200_OK)
+@whoami_router.get("/user", status_code=status.HTTP_200_OK, response_model=s.WhoAmIOut)
 def whoami(current_user: m.User = Depends(get_current_user)):
-    return bool(current_user)
+    return s.WhoAmIOut(uuid=current_user.uuid)
