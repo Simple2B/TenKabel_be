3471ad490529bf9dddbff5838be0deb20c280eff
sa1am8 <tinkerino571@gmail.com>
2023-06-19 11:20:15 +0300

    enum fix

--- app/schema/enums.py
+++ app/schema/enums.py
@@ -2,7 +2,7 @@ import enum
 
 
 class JobStatus(enum.Enum):
-    PENDING = "pending"
-    APPROVED = "approved"
-    IN_PROGRESS = "in_progress"
-    JOB_IS_FINISHED = "job_is_finished"
+    PENDING = 1
+    APPROVED = 2
+    IN_PROGRESS = 3
+    JOB_IS_FINISHED = 4
