f4bec9378c05d80f5f96b2ed802174d578f0cba3
varvara <varvarashcherbyna7@gmail.com>
2023-06-27 09:41:46 +0200

    alembic merge heads

---
+++ migrations/versions/a995b06e4777_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a995b06e4777
+Revises: 362ae768887b, ff3ce51309c9
+Create Date: 2023-06-27 09:40:39.375690
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a995b06e4777'
+down_revision = ('362ae768887b', 'ff3ce51309c9')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
