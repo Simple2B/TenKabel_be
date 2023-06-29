082ba31611f8252fef20b0d8b9d00b898f595dcb
sa1am8 <tinkerino571@gmail.com>
2023-06-29 15:57:12 +0300

    dependency on payplus customer uid added

--- app/dependency/__init__.py
+++ app/dependency/__init__.py
@@ -1,4 +1,4 @@
 # flake8: noqa F401
-from .user import get_current_user, get_user
+from .user import get_current_user, get_user, get_payplus_verified_user
 from .controller import get_mail_client, get_google_client
 from .job import get_job_by_uuid
