d694fecd661bfccefc0a39e00d8b9dfe878bccc4
sa1am8 <tinkerino571@gmail.com>
2023-07-04 11:58:00 +0300

    version app

--- tests/test_routes/test_whoami.py
+++ tests/test_routes/test_whoami.py
@@ -1,13 +1,15 @@
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 from sqlalchemy import select
+from fastapi import status
+
 
 from tests.fixture.test_data import TestData
 import app.model as m
 import app.schema as s
 
 
-def test_auth_user_jobs(
+def test_who_am_i(
     client: TestClient,
     db: Session,
     test_data: TestData,
@@ -20,8 +22,9 @@ def test_auth_user_jobs(
         "api/whoami/user",
         headers={
             "Authorization": f"Bearer {authorized_users_tokens[0].access_token}",
+            "App-Version": "1.0.0",
         },
     )
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
     resp_obj: s.WhoAmIOut = s.WhoAmIOut.parse_obj(response.json())
     assert resp_obj.uuid == user.uuid
