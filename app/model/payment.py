11deaefd2a94dd525ca1b5461f7240d91301375e
sa1am8 <tinkerino571@gmail.com>
2023-09-20 12:07:08 +0300

    staging

---
+++ app/model/payment.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+import sqlalchemy as sa
+from sqlalchemy import orm
+
+from app.database import db
+from app.utility import generate_uuid
+from app import schema as s
+
+
+class Payment(db.Model):
+    __tablename__ = "payments"
+
+    id: orm.Mapped[int] = orm.mapped_column(sa.Integer, primary_key=True)
+    uuid: orm.Mapped[str] = orm.mapped_column(
+        sa.String(36),
+        unique=True,
+        index=True,
+        default=generate_uuid,
+    )
+
+    job_id: orm.Mapped[int] = orm.mapped_column(
+        sa.ForeignKey("jobs.id"), nullable=False
+    )
+    payment_status: orm.Mapped[s.enums.PaymentStatus] = orm.mapped_column(
+        sa.Enum(s.enums.PaymentStatus), default=s.enums.PaymentStatus.UNPAID
+    )
+
+    created_at: orm.Mapped[datetime] = orm.mapped_column(
+        sa.DateTime, default=datetime.utcnow
+    )
