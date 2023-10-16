40614eeb8cc64e3d9ba156ee6dd340c98f5f1d4c
sa1am8 <tinkerino571@gmail.com>
2023-10-16 11:33:50 +0300

    app_review model integrated

---
+++ app/schema/app_review.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from pydantic import BaseModel, Field
+
+
+class AppReviewIn(BaseModel):
+    stars_count: int = Field(None, ge=1, le=5)
+    review: str = Field(..., max_length=1000)
+
+    class Config:
+        orm_mode = True
+        use_enum_values = True
+
+
+class AppReviewOut(AppReviewIn):
+    id: int
+    uuid: str
+    user_id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+        use_enum_values = True
