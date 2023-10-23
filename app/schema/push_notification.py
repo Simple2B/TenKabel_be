727fc3ffefbd22f0ba382f73b7f0f6859e284b89
BohdanSizomin <bohdansizomin@gmail.com>
2023-10-23 15:19:06 +0300

    format time of notifications

--- app/schema/push_notification.py
+++ app/schema/push_notification.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 from .enums import NotificationType
 
@@ -13,6 +13,16 @@ class PushNotificationPayload(BaseModel):
     job_commission: str
     worker_name: str
     owner_name: str
+
+    @validator("job_time", pre=True, always=True)
+    def format_time(cls, value):
+        try:
+            # Attempt to format the input to "YYYY-MM-DD HH:MM"
+            formatted_time = value.strftime("%Y-%m-%d %H:%M")
+            return formatted_time
+        except (AttributeError, ValueError):
+            # If formatting fails, return the original value
+            return value
 
 
 class PushNotificationMessage(BaseModel):
