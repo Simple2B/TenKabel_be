092b93a84eab047ab046d3053cd6205a856553ab
BohdanSizomin <bohdansizomin@gmail.com>
2023-11-06 12:54:57 +0200

    upd of deploy_prod v2

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -2,12 +2,10 @@
 
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-if [ "$CURRENT_BRANCH" != "main" ]; thens
+if [ "$CURRENT_BRANCH" != "main" ]; then
   git checkout main
 fi
 git merge develop
-git pull origin main
-
 git add .
 git commit -m "Update changes"
 git push origin main
